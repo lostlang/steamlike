6c9140b47062eb6008cdfdcf637930eb77feee81
Alec Lang <lostlang@icloud.com>
2023-07-30 02:00:46 +0300

    feat: Add regExp

--- src/routes/api/getImages/+server.ts
+++ src/routes/api/getImages/+server.ts
@@ -4,10 +4,14 @@ import * as fs from "fs";
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET() {
   const address = "./static/images";
+  const regexp = new RegExp(".(jpg|jpeg|png|gif)$");
 
-  const files = fs.readdirSync(address).map((file) => {
-    return "./images/" + file;
-  });
+  const files = fs
+    .readdirSync(address)
+    .filter((file) => regexp.test(file))
+    .map((file) => {
+      return "./images/" + file;
+    });
 
   return json(files);
 }
